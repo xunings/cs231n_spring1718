1f0b709b4397f5f433e948f9da940e72def09de7
xunings <arc.neco@gmail.com>
2019-01-29 15:30:44 +0800

    Complete saliency map.

--- cs231n/classifiers/squeezenet.py
+++ cs231n/classifiers/squeezenet.py
@@ -5,10 +5,15 @@ NUM_CLASSES = 1000
 def fire_module(x,inp,sp,e11p,e33p):
     with tf.variable_scope("fire"):
         with tf.variable_scope("squeeze"):
+            # XN: W is the conv filter with shape [filter_height, filter_width, in_channels, out_channels]
+            # ref: https://www.tensorflow.org/api_docs/python/tf/nn/conv2d
+            # this compresses the channels when inp > sp, and keeps the original H and W.
             W = tf.get_variable("weights",shape=[1,1,inp,sp])
             b = tf.get_variable("bias",shape=[sp])
             s = tf.nn.conv2d(x,W,[1,1,1,1],"VALID")+b
             s = tf.nn.relu(s)
+        # two different conv modules are used in parallel, 1x1 and 3x3 respectively.
+        # the results are concatenated.
         with tf.variable_scope("e11"):
             W = tf.get_variable("weights",shape=[1,1,sp,e11p])
             b = tf.get_variable("bias",shape=[e11p])
@@ -19,6 +24,8 @@ def fire_module(x,inp,sp,e11p,e33p):
             b = tf.get_variable("bias",shape=[e33p])
             e33 = tf.nn.conv2d(s,W,[1,1,1,1],"SAME")+b
             e33 = tf.nn.relu(e33)
+        # XN: the shape is N,H,W,channel. so the output of two modules are concatenated 
+        # in the channel dimension.
         return tf.concat([e11,e33],3)
 
 
@@ -30,8 +37,10 @@ class SqueezeNet(object):
         layers = []
         with tf.variable_scope('features', reuse=reuse):
             with tf.variable_scope('layer0'):
+                # XN: [3,3,3,64] is [filter_height, filter_width, in_channels, out_channels]
                 W = tf.get_variable("weights",shape=[3,3,3,64])
                 b = tf.get_variable("bias",shape=[64])
+                # XN: [1,2,2,1] correponds to the stride of [N,H,W,C]
                 x = tf.nn.conv2d(x,W,[1,2,2,1],"VALID")
                 x = tf.nn.bias_add(x,b)
                 layers.append(x)
@@ -39,8 +48,11 @@ class SqueezeNet(object):
                 x = tf.nn.relu(x)
                 layers.append(x)
             with tf.variable_scope('layer2'):
+                # XN: [1,3,3,1] is the window size correponds to the [N,H,W,C]
                 x = tf.nn.max_pool(x,[1,3,3,1],strides=[1,2,2,1],padding='VALID')
                 layers.append(x)
+            # XN: the input of 64 is from the output of layer0, 
+            # the output is e11p(64)+e33p(64)=128
             with tf.variable_scope('layer3'):
                 x = fire_module(x,64,16,64,64)
                 layers.append(x)
@@ -71,8 +83,11 @@ class SqueezeNet(object):
             with tf.variable_scope('layer12'):
                 x = fire_module(x,512,64,256,256)
                 layers.append(x)
+        # XN: layers includes the x of all layers. 
         return layers
 
+    # XN: __init__ creates the full computational graph while constructing the SqueezeNet class.
+    # __init__ calls extract_features, which is the big black box part of the NN. 
     def __init__(self, save_path=None, sess=None):
         """Create a SqueezeNet model.
         Inputs:
@@ -85,8 +100,10 @@ class SqueezeNet(object):
         self.layers = []
         x = self.image
         self.layers = self.extract_features(x, reuse=False)
+        # XN: the last layer is the layer (N*13*13*512?) before the feature scores.
         self.features = self.layers[-1]
         with tf.variable_scope('classifier'):
+            # XN: this is classifier/layer0, not the features/layer0 used in extract_features
             with tf.variable_scope('layer0'):
                 x = self.features
                 self.layers.append(x)
@@ -100,8 +117,10 @@ class SqueezeNet(object):
                 x = tf.nn.relu(x)
                 self.layers.append(x)
             with tf.variable_scope('layer3'):
+                # XN: the input is H=13, W=13, C=1000?
                 x = tf.nn.avg_pool(x,[1,13,13,1],strides=[1,13,13,1],padding='VALID')
                 self.layers.append(x)
+        # XN: reshape [N,1,1,1000] to N*1000?
         self.scores = tf.reshape(x,[-1, NUM_CLASSES])
 
         if save_path is not None:
